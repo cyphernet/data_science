0d4057870538c57bb80dd960f5407dccebfb6995
Feliks <felix8@gmail.com>
2013-06-17 01:29:52 -0400

    fixed variables

--- 1/1.R
+++ 1/1.R
@@ -6,12 +6,12 @@ s <- gh_data[order(gh_data$pushes, decreasing=T), ]
 s1 <- s[1:120, ]
 
 # Somehow theres a null language, filter it out
-s1[s1$repository_language != "null", ]
+s2 <- s1[s1$repository_language != "null", ]
 
 library("ggplot2")
 
 # Plot the pushes over time
-ggplot(data=gh_data) + aes(x=month, y=pushes, group=repository_language, color=repository_language) + geom_line(aes(group=repository_language)) + geom_point()
+ggplot(data=s2) + aes(x=month, y=pushes, group=repository_language, color=repository_language) + geom_line(aes(group=repository_language)) + geom_point()
 
 # I don't like that one, let's try a bar graph
 ggplot(data=s2) + aes(x=month, y=pushes, fill=repository_language) + geom_bar(stat = "identity") + ggtitle("Github language popularity over time")
